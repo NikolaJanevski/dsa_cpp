772898e36c8120dae0f9e007d16d91d258e7639f
NikolaJanevski <njanevsk@mix.wvu.edu>
2022-08-30 22:50:45 -0400

    Added LIFO class.

---
+++ src/LIFO.hpp
@@ -0,0 +1,49 @@
+#ifndef _LIFO_H
+#define _LIFO_H
+
+#include <memory>
+#include "Interface.hpp"
+
+template <typename E> class LIFO : protected Interface<E> {
+public:
+    LIFO() {
+        _data = std::make_unique<E []>(this->_size);
+    };
+
+    LIFO(int capacity) : Interface<E>(capacity) {
+        _data = std::make_unique<E []>(this->_capacity);
+    };
+
+    ~LIFO() {
+        _data.reset();
+    };
+
+    void add(E value) {
+        if (this->_size >= this->_capacity) {
+            // increase capacity
+            std::unique_ptr<E []> temp = std::make_unique<E []>(this->_capacity * 2);
+            for (int i = 0; i < this->_capacity; i++) {
+                temp[i] = _data[i];
+            }
+            
+            this->_capacity = this->_capacity * 2;
+            
+            _data = std::move(temp);
+        }
+        _data[_top++] = value;
+    }
+
+    E get() {
+        if (_top < 0) {
+            throw std::out_of_range ("Stack is empty");
+        }
+
+        this->_size--;
+        return _data[_top--];
+    }
+private:
+    int _top = -1;
+    std::unique_ptr<E []> _data;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "Interface.hpp"
 #include "FIFO.hpp"
+#include "LIFO.hpp"
 
 using namespace std;
 
 int main() {
     std::cout << "Hello World" << std::endl;
     FIFO<int> queue;
+    LIFO<int> stack;
     return 0;
 }
